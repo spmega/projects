d35af1a9038924ddd2168eaf882bde517c6f0ca1
spmega <spmega4567@gmail.com>
2015-12-23 13:22:27 -0500

    Add first java class

---
+++ src/main/java/com/vpath/threadhardening/Ex1.java
@@ -0,0 +1,13 @@
+package com.vpath.threadhardening;
+
+/**
+ * Created by spmega on 12/23/15.
+ */
+public class Ex1 {
+    public Ex1() {
+    }
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
